714cef7de2e850f1f1eb11e76c8b129d730de47a
SunSoparine <36395963+SunSoparine@users.noreply.github.com>
2018-02-12 17:35:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ bot.on('message', (message) => {
     	if(message.content == 'delete me') {
         message.channel.sendMessage(message.author + '.chr was deleted successfully.')
     }
+	
+
 
 //ily/ihy commands
     	if(message.content == 'I love you Monika') {
@@ -326,6 +328,10 @@ bot.on('message', (message) => {
 
         if(message.content == "Did you ever hear the tragedy of Darth Plagueis the Wise?") {
         message.channel.sendMessage("I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic, he could save others from death, but not himself.")
+    }
+	
+	if(message.content == "It's over Anakin") {
+	message.channel.sendMessage("I have the high ground!")
     }
 });
 
